ce5f19374a99c35fed65d456f234dd6bf700981e
Jen Spinney <jen.spinney@hpe.com>
2016-11-28 17:48:32 -0500

    Revert "Stop timer to avoid suspected race condition in tests"
    This reverts commit 3f7bea9fe637718d3b1de5d1a2360fa578e78e8c.
    
    * It seems this change causes the test we attempted to fix to hang in
    some cases
    
    Signed-off-by: Danny Cao <dcao@us.ibm.com>

--- consuldownchecker/consul_down_checker.go
+++ consuldownchecker/consul_down_checker.go
@@ -43,7 +43,6 @@ func (c *ConsulDownChecker) Run(signals <-chan os.Signal, ready chan<- struct{})
 			logger.Info("received-signal")
 			return nil
 		case <-retryTimer.C():
-			retryTimer.Stop()
 			leaderCheckCounter++
 			hasLeader, err := c.checkForLeader(logger)
 			if err != nil {
