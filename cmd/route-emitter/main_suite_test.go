301c0cbe1b7745e91f125f1112206fd068e58dcc
Nima Kaviani <nkavian@us.ibm.com>
2017-01-05 18:23:36 -0800

    pass ClusterRunnerConfig to NewClusterRunner
    [#136835715]

--- cmd/route-emitter/main_suite_test.go
+++ cmd/route-emitter/main_suite_test.go
@@ -121,9 +121,11 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 	sqlRunner = test_helpers.NewSQLRunner(dbName)
 
 	consulRunner = consulrunner.NewClusterRunner(
-		9001+config.GinkgoConfig.ParallelNode*consulrunner.PortOffsetLength,
-		1,
-		"http",
+		consulrunner.ClusterRunnerConfig{
+			StartingPort: 9001 + config.GinkgoConfig.ParallelNode*consulrunner.PortOffsetLength,
+			NumNodes:     1,
+			Scheme:       "http",
+		},
 	)
 
 	logger = lagertest.NewTestLogger("test")
