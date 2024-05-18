920ad0f3c34d4a8fea70ebf2936c184ad5365e9d
Andrew Wittrock <andrew.wittrock@broadcom.com>
2024-05-18 01:03:00 +0000

    Update go-loggregator to v9

--- cmd/route-emitter/main_suite_test.go
+++ cmd/route-emitter/main_suite_test.go
@@ -18,7 +18,7 @@ import (
 	"code.cloudfoundry.org/bbs/test_helpers/sqlrunner"
 	"code.cloudfoundry.org/diego-logging-client/testhelpers"
 	"code.cloudfoundry.org/durationjson"
-	"code.cloudfoundry.org/go-loggregator/v8/rpc/loggregator_v2"
+	"code.cloudfoundry.org/go-loggregator/v9/rpc/loggregator_v2"
 	"code.cloudfoundry.org/inigo/helpers/certauthority"
 	"code.cloudfoundry.org/inigo/helpers/portauthority"
 	"code.cloudfoundry.org/lager/v3/lagerflags"
